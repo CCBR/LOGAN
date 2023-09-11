f334c97e984dcff76e8c0dbbf4bd77a0560cccd5
dnousome <dnousome@gmail.com>
2023-09-11 13:13:44 -0400

    fix: renamed docker build files

--- docker/logan_base/build.sh
+++ docker/logan_base/build.sh
@@ -1,14 +1,14 @@
 # Build image
-docker buildx build -f Dockerfile -t ccbr_wgs_base:v0.3.0 .
+docker buildx build -f Dockerfile -t ccbr_logan_base:v0.3.0 .
 
 # Tag image with version and reset latest
-docker tag ccbr_wgs_base:v0.3.0 dnousome/ccbr_wgs_base:v0.3.0
-docker tag ccbr_wgs_base:v0.3.0 dnousome/ccbr_wgs_base
+docker tag ccbr_logan_base:v0.3.0 dnousome/ccbr_logan_base:v0.3.0
+docker tag ccbr_logan_base:v0.3.0 dnousome/ccbr_logan_base
 #docker tag ccbr_wgs_base:v0.1.0 nciccbr/ccbr_wgs_base:v0.1.0
 #docker tag ccbr_wgs_base:v0.1.0 nciccbr/ccbr_wgs_base
 
 # Push image to DockerHub
-docker push dnousome/ccbr_wgs_base:v0.3.0 ##Push to v0.3.0 branch
-docker push dnousome/ccbr_wgs_base:latest ##Push to Latest tag
+docker push dnousome/ccbr_logan_base:v0.3.0 ##Push to v0.3.0 branch
+docker push dnousome/ccbr_logan_base:latest ##Push to Latest tag
 #docker push nciccbr/ccbr_wgs_base:v0.1.0
 #docker push nciccbr/ccbr_wgs_base:latest
