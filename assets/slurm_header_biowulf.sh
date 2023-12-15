724f0edc916ea54d95858922f2fc919bf7b7b46c
Darryl Nousome <dnousome@gmail.com>
2023-12-15 09:51:46 -0500

    fix: rename slurm

--- assets/slurm_header_biowulf.sh
+++ assets/slurm_header_biowulf.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=1g
 #SBATCH --time=1-00:00:00
 #SBATCH --parsable
-#SBATCH -J "tool_name"
+#SBATCH -J "LOGAN"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output "log/slurm_%j.log"
 #SBATCH --output "log/slurm_%j.log"
